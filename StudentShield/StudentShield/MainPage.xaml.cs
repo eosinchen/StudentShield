b048c79d106d6b8e072f7ff78ca18e40412e4042
Eosin Chen <eosinchen@gmail.com>
2018-11-28 17:32:36 +0800

    初始版本

--- StudentShield/StudentShield/MainPage.xaml.cs
+++ StudentShield/StudentShield/MainPage.xaml.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Plugin.Messaging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Essentials;
 
 namespace StudentShield
 {
@@ -13,5 +15,35 @@ namespace StudentShield
         {
             InitializeComponent();
         }
+
+        async void OnbtnWarningClick(object sender, EventArgs args)
+        {
+            await DisplayAlert("Title", "嚇阻鈴聲", "是", "否");
+        }
+
+        async void OnbtnMessageClick(object sender, EventArgs args)
+        {
+            await DisplayAlert("Title", "訊息通報", "是", "否");
+        }
+
+        async void OnbtnTelephoneClick(object sender, EventArgs args)
+        {
+            // await DisplayAlert("Title", "電話通報", "是", "否");
+            var call = CrossMessaging.Current.PhoneDialer;
+
+            if (call.CanMakePhoneCall)
+                call.MakePhoneCall("12345678");
+        }
+
+        async void OnbtnRecordClick(object sender, EventArgs args)
+        {
+            // await DisplayAlert("Title", "蒐證紀錄", "是", "否");
+            var photo = await Plugin.Media.CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions() { });
+
+            if (photo != null)
+                // PhotoImage.Source = ImageSource.FromStream(() => { return photo.GetStream(); });
+                ImageSource.FromStream(() => { return photo.GetStream(); });
+        }
+
     }
 }
